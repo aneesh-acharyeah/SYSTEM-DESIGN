a571255038e7f314242abfd90c2310a85ca44171
Aneesh <aneeshg2001@gmail.com>
2025-05-27 08:26:40 +0530

    files updated

--- image-slider/script.js
+++ image-slider/script.js
@@ -1,23 +1,24 @@
 const slides = document.querySelectorAll(".slide");
 const prevBtn = document.querySelector(".prev");
-const nestBtn = document.querySelector(".next");
+const nextBtn = document.querySelector(".next");
 
 let currentSlide = 0;
 
 function showSlide(index) {
-    slides.forEach((slide, i) => {
-        slide.classList.remove("active");
-        if (i === index) {
-            slide.classList.add("active");
-        }
-    })
+  slides.forEach((slide, i) => {
+    slide.classList.remove("active");
+    if (i === index) {
+      slide.classList.add("active");
+    }
+  });
 }
 
 nextBtn.addEventListener("click", () => {
-    currentSlide = (currentSlide + 1) % slides.length;
-    showSlide(currentSlide)
-})
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+});
+
 prevBtn.addEventListener("click", () => {
-    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-    showSlide(currentSlide);
-})
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+});
