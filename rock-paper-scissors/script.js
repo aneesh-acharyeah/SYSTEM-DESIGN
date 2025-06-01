f8966303a16dc9c35aea1e66d420f13cf04a1461
Aneesh <aneeshg2001@gmail.com>
2025-06-01 22:28:53 +0530

    files updated

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,4 +1,4 @@
-const choices = ['rock', 'paper', 'scissors'];
+const choices = ["rock", "paper", "scissors"];
 const buttons = document.querySelectorAll("button[data-choice]");
 const message = document.getElementById("message");
 const playerScoreEl = document.getElementById("player-score");
@@ -8,33 +8,32 @@ let playerScore = 0;
 let computerScore = 0;
 
 buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-        const playerChoice = button.getAttribute("data-choice");
-        const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-        playRound(playerChoice, computerChoice)
-    })
-})
+  button.addEventListener("click", () => {
+    const playerChoice = button.getAttribute("data-choice");
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    playRound(playerChoice, computerChoice);
+  });
+});
 
 function playRound(player, computer) {
-    if (player === computer) {
-        message.textContent = `It's a draw! You both chose ${player}`;
-        return;
-    }
-    const winConditions = {
-        rock: "scissors",
-        paper: "rock",
-        scissors: "paper"
-    }
+  if (player === computer) {
+    message.textContent = `It's a draw! You both chose ${player}.`;
+    return;
+  }
 
-    if (winConditions[player === computer]) {
-        playerScore++;
-        playerScoreEl.textContent = playerScore;
-        message.textContent = `Yout win! ${player} beats ${computer}`;
-    } else {
-        computerScore++;
-        computerScoreEl.textContent = computerScore;
-        message.textContent = `You lose! ${computer} beats ${player}`
-    }
+  const winConditions = {
+    rock: "scissors",
+    paper: "rock",
+    scissors: "paper",
+  };
 
-
+  if (winConditions[player] === computer) {
+    playerScore++;
+    playerScoreEl.textContent = playerScore;
+    message.textContent = `You win! ${player} beats ${computer}.`;
+  } else {
+    computerScore++;
+    computerScoreEl.textContent = computerScore;
+    message.textContent = `You lose! ${computer} beats ${player}.`;
+  }
 }
