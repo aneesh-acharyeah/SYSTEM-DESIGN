85fa9b71be9fd75db8ccbbd0170ee669caf41ece
Aneesh <aneeshg2001@gmail.com>
2025-06-01 22:27:08 +0530

    files updated

---
+++ rock-paper-scissors/script.js
@@ -0,0 +1,40 @@
+const choices = ['rock', 'paper', 'scissors'];
+const buttons = document.querySelectorAll("button[data-choice]");
+const message = document.getElementById("message");
+const playerScoreEl = document.getElementById("player-score");
+const computerScoreEl = document.getElementById("computer-score");
+
+let playerScore = 0;
+let computerScore = 0;
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const playerChoice = button.getAttribute("data-choice");
+        const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+        playRound(playerChoice, computerChoice)
+    })
+})
+
+function playRound(player, computer) {
+    if (player === computer) {
+        message.textContent = `It's a draw! You both chose ${player}`;
+        return;
+    }
+    const winConditions = {
+        rock: "scissors",
+        paper: "rock",
+        scissors: "paper"
+    }
+
+    if (winConditions[player === computer]) {
+        playerScore++;
+        playerScoreEl.textContent = playerScore;
+        message.textContent = `Yout win! ${player} beats ${computer}`;
+    } else {
+        computerScore++;
+        computerScoreEl.textContent = computerScore;
+        message.textContent = `You lose! ${computer} beats ${player}`
+    }
+
+
+}
