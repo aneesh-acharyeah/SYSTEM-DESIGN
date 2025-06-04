6947b926fe317394ee65a1465fdbb30436d0555a
Aneesh <aneeshg2001@gmail.com>
2025-06-04 07:57:15 +0530

    error fixex

--- scroll-progress/script.js
+++ scroll-progress/script.js
@@ -1,7 +1,7 @@
 window.addEventListener('scroll', () => {
-    const scrollTop = document.documentElement.scrollTop;
-    const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    const scrollPercent = (scrollTop / scrollHeight) * 100;
+  const scrollTop = document.documentElement.scrollTop;
+  const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const scrollPercent = (scrollTop / scrollHeight) * 100;
 
-    document.getElementById('progress-bar').style.width = `${scrollPercent}`
-})
+  document.getElementById('progress-bar').style.width = `${scrollPercent}%`;
+});
